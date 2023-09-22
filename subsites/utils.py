a90b30547ce4c39420db28b7038a948321d9fe65
mattiagiupponi <mattia.giupponi@gmail.com>
2023-09-22 16:09:44 +0200

    [Fixes #2] Add description for render function

--- subsites/utils.py
+++ subsites/utils.py
@@ -1,9 +1,9 @@
+import os
 from django.conf import settings
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404
-from django.template import TemplateDoesNotExist
 from django.core.exceptions import ImproperlyConfigured
-
+from subsites import project_dir
+from django.template.backends.django import DjangoTemplates
 from subsites.models import SubSite
 
 
@@ -29,52 +29,66 @@ def extract_subsite_slug_from_request(request, return_object=True):
     return None
 
 
-def subsite_get_template(template_name, using=None, slug=None):
+def subsite_render_to_string(template_name, context=None, request=None, using=None, slug=None):
     """
-    Load and return a template for the given name.
-
-    Raise TemplateDoesNotExist if no such template exists.
+    Dynamically load the template from the subsite template folder.
+    A new "engine" object is initilized and the template directory for 
+    the single subsite is injected to the template engine can find
+    the proper subsite-template.
+    The engine is going to be initilized everytime so that
+    we can dynamically load the subsite template.
+    The subsite template structure must match the default geonode one
     """
-    from subsites import project_dir
-    from django.template.backends.django import DjangoTemplates
+    # creating the subsite template path
     _path = f"{project_dir}/templates/{slug}/"
     payload = {}
+    # retrieve the settings information
     options = subsite_get_settings()
+    # we want to use ONLY the default geonode template
+    # then we can copy the params from it to init the template engine
     for template in options:
         if template == 'GeoNode Project Templates':
             payload = options[template].copy()
             break
-
+    # the key backend is not needed so we pop it out
     payload.pop('BACKEND')
-    payload['DIRS'].insert(0, _path)
+    # if the subsite template path exsits, we will add it 
+    # as first in the template dir lists so it will match during
+    # the  rendering
+    if os.path.exists(_path):
+        payload['DIRS'].insert(0, _path)
 
+    # we initiate the Template Engine with the above payload
     engine = DjangoTemplates(payload)
 
-    return engine.get_template(template_name)
-
-
-def subsite_render_to_string(template_name, context=None, request=None, using=None, slug=None):
-    """
-    Load a template and render it with a context. Return a string.
-
-    template_name may be a string or a list of strings.
-    """
-    template = subsite_get_template(template_name, using=using, slug=slug)
+    template = engine.get_template(template_name)
+    
+    # after getting the template, we can finally render it
+    # the subsite template will match since is first in the dirs order
     return template.render(context, request)
 
 
 
 def subsite_render(request, template_name, context=None, content_type=None, status=None, using=None, slug=None):
     """
-    Return a HttpResponse whose content is filled with the result of calling
-    django.template.loader.render_to_string() with the passed arguments.
+    Used instead of the default django render function.
+    This is needed since we can dynamically load the subsite
+    template based on the request coming from the view
+    It will return the HttpResponse with the template
+    for the selected subsite
     """
-
     content = subsite_render_to_string(template_name, context, request, using=using, slug=slug)
     return HttpResponse(content, content_type, status)
 
 
 def subsite_get_settings():
+    '''
+    Mock of the django function used during the startup
+    to initialize the template engines. 
+    The function will read the settings.TEMPLATE variable
+    and generates a payload with all the information needed
+    to successfully load the template (context processors, app_dirs) etc...
+    '''
     _templates = settings.TEMPLATES
     templates = {}
     for tpl in _templates:
@@ -96,5 +110,7 @@ def subsite_get_settings():
             **tpl,
         }
         templates[tpl['NAME']] = tpl
-
+    # return a payload with the default geonode project template
+    # and all the default context_processors, app_dirs etc...
+    # required for load the template
     return templates
